996f606b92fd73b5a6a0d1f1faef5bfa639e0cb5
Lilian Li <lil712@hotmail.com>
2023-08-11 15:33:30 +0800

    made plant new and create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   root to: 'gardens#index'
-  resources :gardens
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :gardens do
+    resources :plants, only: [:new, :create]
+  end
 end
